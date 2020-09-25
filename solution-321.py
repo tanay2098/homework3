a16eac3cb11f65be5cffa5b89e82c665b74f6fd7
Tanay <tanay.patel@student.csulb.edu>
2020-09-25 08:58:27 +0530

    commit2

--- solution-321.py
+++ solution-321.py
@@ -2,23 +2,32 @@ a=0
 b=0
 c=0
 d=0
-amt=int(input('Enter a price of one dollar or less: '))
+dollar=100
+user_amt=int(input('Enter a price in cents: '))
+change=dollar-user_amt
 
-if amt>=25:
-    a=amt/25
-    amt=amt%25
+if user_amt==dollar:
+    print('No change due')
+else:
+    print('Your Change is:')
 
-if amt>=10:
-    b=amt/10
-    amt=amt%10
+if change>=25:
+    a=(change//25)
+    change=change%25
+    print(a,'quarters')
 
-if amt>=5:
-    c=amt/5
-    amt=amt%5
+if change>=10:
+    b=(change//10)
+    change=change%10
+    print(b,'dimes')
 
-if amt>0:
-    d=amt/1
-    amt=0
+if change>=5:
+    c=(change//5)
+    change=change%5
+    print(c,'nickles')
 
-print('Your Change is: \n',a, 'quarters\n',b, 'dimes\n',c, 'nickles\n',d, 'pennies') 
+if change>0:
+    d=(change//1)
+    change=0
+    print(d,'pennies')
 
