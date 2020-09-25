def04a27324a5fb56018fa358757c512450265f9
Tanay <tanay.patel@student.csulb.edu>
2020-09-25 12:56:25 +0530

    commit3

--- solution-317.py
+++ solution-317.py
@@ -8,7 +8,7 @@ for row in range(0,10):
     print('')
 
 
-print('(b)')
+print('\n(b)')
 
 for row in range(10,0,-1):
 
@@ -17,6 +17,32 @@ for row in range(10,0,-1):
         print("*",end='')
     print('')
 
+print('\n(c)')
 
+row=1
+while(row<=10):
+    col=1
+    while(col<=10):
+        if(col<row):
+            print(' ',end='')
+        else:
+            print('*',end='')
+        col+=1
+    row+=1
+    print('')
 
+print('\n(d)')
 
+r=10
+k = 2*r - 1
+    
+for i in range(0, r): 
+       
+    for j in range(0, k): 
+        print(end=' ') 
+    k = k - 1
+        # inner loop to handle number of columns  
+    for j in range(0, i+1):   
+        print("*", end="") 
+       
+    print()
